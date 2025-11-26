b371559e7912034a57ce8828502eecc4f1376fa9
MikeGX30 <1224100689.mla@gmail.com>
2025-11-26 13:31:45 -0600

    arbol

---
+++ NodoArbol.java
@@ -0,0 +1,29 @@
+package ArbolBinario;
+
+/**
+ * @author GTID141 
+ * @author 1224100689.mla@gmail.com
+ * @author miguel Lozano
+ */
+public class NodoArbol {
+    private int dato;
+
+ public NodoArbol hijoIzquierdo;
+ public NodoArbol hijoDerecho;
+ 
+ public NodoArbol(int valor) {
+    this.dato = valor;
+    this.hijoIzquierdo = hijoIzquierdo;
+    this.hijoDerecho = hijoDerecho;
+ }
+
+ 
+ public int getDato() {
+ return dato;
+ }
+
+ public void setDato(int nuevoDato) {
+ this.dato = nuevoDato;
+ }
+}
+
